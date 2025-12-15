8cf817648925531d47d7962f9729da0ccbeb7408
Him Chan <him.c.chan.-nd@disney.com>
2025-12-15 10:59:59 +0800

    Refactor string reference field to support dynamic id and path extraction
    - Introduced a new `extractValue` function to handle both static and dynamic values for `id` and `path`.
    - Updated the `stringRefAdditionalField` component to utilize the new extraction logic, improving flexibility in handling entity references.

--- src/additional_fields/string_reference.tsx
+++ src/additional_fields/string_reference.tsx
@@ -6,28 +6,38 @@ import {
   ReferencePreview,
 } from '@firecms/core';
 
+type Extractor<T extends Record<string, unknown>> = (entity: Entity<T>) => string;
+
+function extractValue<T extends Record<string, unknown>>(value: string | Extractor<T>, entity: Entity<T>): string {
+  return typeof value === 'function' ? value(entity) : value;
+}
+
 export const stringRefAdditionalField = <T extends Record<string, unknown>>({
   key,
   name,
+  id = (entity: Entity<T>) => entity.id,
   path,
-  idExtractor = (entity: Entity<T>) => entity.id,
   width,
   size = 'medium',
 }: {
   key: string;
   name: string;
-  path: string;
-  idExtractor?: (entity: Entity<T>) => string;
+  id: string | Extractor<T>;
+  path: string | Extractor<T>;
   width?: number;
   size?: PreviewSize;
 }): AdditionalFieldDelegate<T> => ({
   key,
   name,
   width,
-  Builder: ({ entity }) => (
-    <ReferencePreview
-      size={size}
-      reference={new EntityReference(idExtractor(entity), path)}
-    />
-  ),
+  Builder: ({ entity }) => {
+    const idValue = extractValue(id, entity);
+    const pathValue = extractValue(path, entity);
+
+    return (
+      <ReferencePreview
+        size={size}
+        reference={new EntityReference(pathValue, idValue)} />
+    );
+  },
 });
